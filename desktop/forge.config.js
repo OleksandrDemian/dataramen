2018e167238873a54f376bb5135f1488288e73d7
odemain <oleksandrovsky@gmail.com>
2026-01-08 19:54:48 +0100

    - add windows publish

--- desktop/forge.config.js
+++ desktop/forge.config.js
@@ -1,10 +1,24 @@
 module.exports = {
   packagerConfig: {
     icon: 'assets/icon',
+    osxSign: false,
+    osxNotarize: false,
+
+    win32metadata: {
+      CompanyName: "DataRamen",
+      FileDescription: "DataRamen",
+      ProductName: "DataRamen",
+    }
   },
   makers: [
     {
       name: '@electron-forge/maker-dmg',
+      platforms: ['darwin'],
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-squirrel',
+      platforms: ['win32'],
       config: {},
     },
   ],
